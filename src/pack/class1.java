17514ca0268764f3507a7de9fac06d2f1568b7cc
Your Name <you@example.com>
2019-10-01 13:07:24 +0530

    First commit

---
+++ src/pack/class1.java
@@ -0,0 +1,22 @@
+package pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class class1 {
+	public static WebDriver driver;
+	public static String URL="https://www.flipkart.com/";
+	 
+  @Test(priority=1)
+  public void openbrowser() throws InterruptedException {
+	  
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\Koothabiran\\eclipse-workspace\\study\\drivers\\chromedriver (2).exe");
+	 driver = new ChromeDriver();
+	  driver.get(URL);
+	  driver.manage().window().maximize();
+	 Thread.sleep(5000);
+	  
+	  
+  }
+}
---
+++ src/pack/class2.java
@@ -0,0 +1,25 @@
+package pack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class class2 {
+	
+	public static WebDriver driver;
+	public static String URL="https://www.amazon.com/";
+	 
+  @Test(priority=1)
+  public void openbrowser() throws InterruptedException {
+	  
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\Koothabiran\\eclipse-workspace\\study\\drivers\\chromedriver (2).exe");
+	 driver = new ChromeDriver();
+	  driver.get(URL);
+	  driver.manage().window().maximize();
+	 Thread.sleep(5000);
+	
+	 driver.close();
+	 
+  }
+
+}
